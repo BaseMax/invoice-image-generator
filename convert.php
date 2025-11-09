efce9f177617089367f98b5ed3da4c5f4347de9a
Your Name <you@example.com>
2025-11-09 16:33:52 +0000

    add another excel images, etc

--- convert.php
+++ convert.php
@@ -8,10 +8,10 @@ require 'vendor/autoload.php';
 
 $products = json_decode(file_get_contents("all-products.json"), true);
 
-$filePath = 'input.xlsx';
+$filePath = 'input2.xlsx';
 if (!file_exists($filePath)) die("input.xlsx not found\n");
 
-$outDir = "tables/";
+$outDir = "tables2/";
 if (!is_dir($outDir)) {
     @mkdir($outDir, 0755, true);
 }
