d7af2bd65b867cb5e91b3b43ed87f6feaaaa92db
Seyyed Ali Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-11-19 19:46:12 +0330

    update

--- create.php
+++ create.php
@@ -79,8 +79,13 @@ function create_product_image(string $code, string $title, string $description, 
 // ---------------------------------------------------------
 // Iterate over extracted tables
 // ---------------------------------------------------------
+$number_sofar = 0;
 foreach ($tables as $tIndex => $table) {
+    $number_sofar++;
     if (empty($table)) continue;
+    if ($number_sofar > 60) {
+        exit();
+    }
 
     $image = $outDir . "table_{$tIndex}.png";
     if (!file_exists($image)) {
